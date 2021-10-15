37d6c47a34470c2b6ddda6574ea53b39c656b5df
harshitScript <85029089+harshitScript@users.noreply.github.com>
2021-10-15 10:44:51 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavigationBar from "./Components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <div>
+        <NavigationBar />
+      </div>
+      <div></div>
     </div>
   );
 }
 
 export default App;
+
+// WEC SHOULD ALWAYS MAKE OUR APPLICATION LAYOUT USING GRID-LAYOUT
